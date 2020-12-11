c077e7de7486d3bee9c90f91f84b0d4499832067
unknown <rwwalla2@illinois.edu>
2020-12-11 12:35:54 -0600

    moving db to its own repo

--- backend/scripts/03_InsertDummyAssets.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO KP_Asset (asset_id, uuid, asset_name, description, creator_id, is_public,path,config)
-VALUES (111301, '21690b4e-5a08-4a03-8caa-7d0aca117277', 'Dummy Asset 1', '', 1, TRUE, 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/2CylinderEngine/glTF-Embedded/2CylinderEngine.gltf', '{}'),
-       (111304, '85f7d0fe-927e-4e81-b019-6b3d08b53ea1', 'Dummy Asset 2', '', 1, TRUE, 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/2CylinderEngine/glTF-Embedded/2CylinderEngine.gltf', '{}'),
-       (111305, '516dc312-7650-4fea-93ad-bd64f89a72da', 'Dummy Asset 3', '', 1, TRUE, 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/2CylinderEngine/glTF-Embedded/2CylinderEngine.gltf', '{}')
