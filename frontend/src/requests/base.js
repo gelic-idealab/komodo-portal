d9dc9058fe2fda8b60cbca123ebd15016023820c
parseccentric <8165314+parseccentric@users.noreply.github.com>
2022-03-28 19:15:57 -0500

    removed zero-width spaces (what the heck??); ignored silly rule

--- frontend/src/requests/base.js
+++ frontend/src/requests/base.js
@@ -11,20 +11,25 @@ const toCamel = data => {
   if (typeof data !== "object" || data === null) {
     return data;
   }
+
   if (data instanceof Array) {
     return data.map(value => toCamel(value));
   }
+
   const newData = {};
   for (const key in data) {
+    // eslint-disable-next-line
     if (data.hasOwnProperty(key)) {
       const splitted = key.split("_");
       let newKey = splitted[0];
       for (let i = 1; i < splitted.length; i++) {
         newKey += splitted[i][0].toUpperCase() + splitted[i].slice(1);
       }
+
       newData[newKey] = toCamel(data[key]);
     }
   }
+
   return newData;
 };
 
